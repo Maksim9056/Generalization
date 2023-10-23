b50d725eed52f283a2bebe57963ada77ece47cf0
Maksim <bobreczovm@inbox.ru>
2023-10-23 12:10:59 +0300

    Добавьте файлы проекта.

---
+++ Обобщение/Студент.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обобщение
+{
+
+    /// <summary>
+    /// Класс студент
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Студент<T>
+    {
+
+        /// <summary>
+        /// Имя студента
+        /// </summary>
+        public string Имя { get; set; }
+
+        /// <summary>
+        /// Оценка студента
+        /// </summary>
+        public T Оценка { get; set; }
+
+        /// <summary>
+        /// Конструктор класса 
+        /// </summary>
+        /// <param name="имя"></param>
+        /// <param name="оценка"></param>
+        public Студент(string имя, T оценка)
+        {
+            Имя = имя;
+            Оценка = оценка;
+        }
+
+
+        /// <summary>
+        /// Метод показывает информацию о студенте
+        /// </summary>
+        public void ПоказатьИнформацию()
+        {
+            Console.WriteLine($"Студент: {Имя}, Оценка: {Оценка}");
+        }
+    }
+
+}
+
